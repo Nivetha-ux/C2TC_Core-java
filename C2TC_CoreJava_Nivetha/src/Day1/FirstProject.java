fd1add183b5433558969cb61a9971ec41794c978
Nivetha-ux <nive13114@gmail.com>
2025-08-18 11:47:08 +0530

    Commit

---
+++ C2TC_CoreJava_Nivetha/src/Day1/FirstProject.java
@@ -0,0 +1,11 @@
+package Day1;
+
+public class FirstProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.print("ITS HERO TIME");
+	}
+
+}
+  
